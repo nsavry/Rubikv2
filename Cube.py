9871c92f1011e851748d00b7dc8d7cdbd77f320a
Nicolas SAVRY <nsavry@e2r4p17.42.fr>
2015-10-23 19:41:00 +0200

    add move + display

--- Cube.py
+++ Cube.py
@@ -6,7 +6,7 @@
 #    By: nsavry <nsavry@student.42.fr>              +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2015/10/20 15:45:20 by nsavry            #+#    #+#              #
-#    Updated: 2015/10/23 16:43:31 by nsavry           ###   ########.fr        #
+#    Updated: 2015/10/23 19:40:37 by nsavry           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -21,15 +21,36 @@ class Cube:
 					[-5,2,-6],[3,2,-6],[3,-4,-6],[-5,-4,-6]]
 		self.fixed = [[0,0,1],[0,0,-6],[0,2,0],[0,-4,0],[3,0,0],[-5,0,0]]
 		self.cube = self.edge + self.corner + self.fixed
+		self.move_tab = {"U": [1, 2, 0, True, 1], "U'": [1, 0, 2, True, 1], "U2": [1, 2, 0, True, 2],
+						"F": [2, 0, 1, True, 1], "F'": [2, 1, 0, True, 1], "F2": [2, 0, 1, True, 2],
+						"R": [0, 1, 2, True, 1], "R'": [0, 2, 1, True, 1], "R2": [0, 1, 2, True, 2],
+						"L": [0, 1, 2, False, 1], "L'": [0, 2, 1, False, 1], "L2": [0, 1, 2, False, 2],
+						"B": [2, 0, 1, False, 1], "B'": [2, 1, 0, False, 1], "B2": [2, 0, 1, False, 2],
+						"D": [1, 2, 0, False, 1], "D'": [1, 0, 2, False, 1], "D2": [1, 2, 0, False, 2]}
 	
 	def display(self):
 		for i in range(-1,2):
-			sys.stdout.write("             ")
+			sys.stdout.write("            ")
 			for j in range(-1,2):
-				self.display_color(self.found_color(j, 1, i)[1])
+				self.display_color(self.find_sticker(j, 1, i)[1])
+			print "\n"
+		for j in range(-1,2):
+			for i in range(-1,2):
+				self.display_color(self.find_sticker(-1, -j, i)[0])
+			for i in range(-1,2):
+				self.display_color(self.find_sticker(i, -j, 1)[2])
+			for i in range(-1,2):
+				self.display_color(self.find_sticker(1, -j, -i)[0])
+			for i in range(-1,2):
+				self.display_color(self.find_sticker(-i, -j, -1)[2])
+			print "\n"
+		for i in range(-1,2):
+			sys.stdout.write("            ")
+			for j in range(-1,2):
+				self.display_color(self.find_sticker(j, -1, -i)[1])
 			print "\n"
 
-	def found_color(self, x, y, z):
+	def find_sticker(self, x, y, z):
 		for sticker in self.cube:
 			if ((sticker[0] == 0 and x == 0) or (sticker[0] != 0 and sticker[0] / abs(sticker[0])) == x):
 				if ((sticker[1] == 0 and y == 0) or (sticker[1] != 0 and sticker[1] / abs(sticker[1])) == y):
@@ -37,25 +58,24 @@ class Cube:
 						return sticker
 
 	def display_color(self, color):
-		i = " "
-		j = " "
+		i = "  "
 		color = abs(color)
-		if color == 1:
-			sys.stdout.write("\033[42m\033[30m" + str(i) + "\033[0m")
-			sys.stdout.write("\033[42m\033[30m" + str(j) + "\033[0m")
-		if color == 2:
-			sys.stdout.write("\033[44m\033[30m" + str(i) + "\033[0m")
-			sys.stdout.write("\033[44m\033[30m" + str(j) + "\033[0m")
-		if color == 3:
-			sys.stdout.write("\033[47m\033[30m" + str(i) + "\033[0m")
-			sys.stdout.write("\033[47m\033[30m" + str(j) + "\033[0m")
-		if color == 4:
-			sys.stdout.write("\033[43m\033[30m" + str(i) + "\033[0m")
-			sys.stdout.write("\033[43m\033[30m" + str(j) + "\033[0m")
-		if color == 5:
-			sys.stdout.write("\033[41m\033[30m" + str(i) + "\033[0m")
-			sys.stdout.write("\033[41m\033[30m" + str(j) + "\033[0m")
-		if color == 6:
-			sys.stdout.write("\033[48;5;202m\033[30m" + str(i) + "\033[0m")
-			sys.stdout.write("\033[48;5;202m\033[30m" + str(j) + "\033[0m")
+		tab = ["42", "47", "41", "43", "48;5;202", "44"]
+		sys.stdout.write("\033[" + tab[color-1] + "m\033[30m" + str(i) + "\033[0m")
 		sys.stdout.write("  ")
+	
+	def move(self, tab):
+		for _ in range(tab[4]):
+			i = 0
+			for sticker in self.cube:
+				if tab[3] == True:
+					if sticker[tab[0]] > 0:
+						tmp = self.cube[i][tab[1]]
+						self.cube[i][tab[1]] = self.cube[i][tab[2]]
+						self.cube[i][tab[2]] = -tmp
+				else:
+					if sticker[tab[0]] < 0:
+						tmp = self.cube[i][tab[2]]
+						self.cube[i][tab[2]] = self.cube[i][tab[1]]
+						self.cube[i][tab[1]] = -tmp
+				i += 1
--- Rubik.py
+++ Rubik.py
@@ -6,7 +6,7 @@
 #    By: nsavry <nsavry@student.42.fr>              +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2015/10/20 14:44:29 by nsavry            #+#    #+#              #
-#    Updated: 2015/10/23 16:12:36 by nsavry           ###   ########.fr        #
+#    Updated: 2015/10/23 19:29:56 by nsavry           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -15,16 +15,18 @@ import Cube
 
 test = Cube.Cube()
 test.init()
-#try:
-#	args = sys.argv[1].upper().split(" ")
-#except:
-#	print "ERROR: Sequence required"
-#	quit()
-#for av in args:
-#	try:
-#		test.tab[av]()
-#	except:
-#		print "ERROR: Wrong Move: '" + av + "'"
-#		quit()
-#print ""
+try:
+	args = sys.argv[1].upper().split(" ")
+except:
+	print "ERROR: Sequence required"
+	quit()
+for av in args:
+	try:
+		print av
+		print test.move_tab[av]
+		test.move(test.move_tab[av])
+	except:
+		print "ERROR: Wrong Move: '" + av + "'"
+		quit()
+print ""
 test.display()
